6760aabbc469c11d3f4dcabba749f85f2396d6e3
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2024-11-28 06:28:30 +0900

    update the detection threshold, modify to show detected material name

--- main_size_detection.py
+++ main_size_detection.py
@@ -1,16 +1,16 @@
 from ultralytics import YOLO
 import cv2
-from collections import deque # to look into the past 5 frames
+
 # load yolov8 model
 model = YOLO('yolov8n.pt')
 
 # load video
-video_path = 0 # webcam
-# video_path = './video1.MOV'
+# video_path = 0 # webcam
+video_path = r'C:\Users\chawt\Desktop\IoT\Code\test_video\homeplus_shot2.mp4'
 cap = cv2.VideoCapture(video_path)
 
 
-# store the bounding boxes of the first detected object in last 10 previous frame
+# store the bounding boxes of the first detected object in previous frame
 previous_sizes = {} # a dict has been used to associate IDs with corresponding sizes
 
 
@@ -33,13 +33,12 @@ while ret:
             # object id to make sure that bounding boxes of the same object are compared later
             object_id = int(box.id[0])
 
+            # TO SHOW THE OBJECT NAME instead of just an id
+            # get the class id and map it to the class name
+            class_id = int(box.cls[0])
+            class_name = model.names[class_id]  # map class ID to class name
 
-            # TODO: record the size of the individual bounding boxes in previous frames and compare the difference
 
-            #print(f"Object ID {object_id}: width {width}, height {height}")
-
-
-            # use deque to keep track of last
             # Check if the object has been seen before (ID already in previous_sizes)
             if object_id in previous_sizes:
                 last_size = previous_sizes[object_id]
@@ -47,18 +46,17 @@ while ret:
                 height_diff = abs(height - last_size[1])
 
                 # If the difference in size exceeds a threshold, it's a significant change
-                if width_diff > 20 or height_diff > 20:
+                if width_diff > 20 or height_diff > 10:
                     print(
-                        f"Significant size change detected for Object {object_id}! Width diff: {width_diff}, Height diff: {height_diff}")
-
+                        f"Significant size change detected for {class_name} (Object ID {object_id})! "
+                        f"Width diff: {width_diff}, Height diff: {height_diff}"
+                    )
             # Update the previous size for this object ID
             previous_sizes[object_id] = (width, height)
 
 
 
         # plot results
-        # cv2.rectangle
-        # cv2.putText
         frame_ = results[0].plot()
 
         # visualize
